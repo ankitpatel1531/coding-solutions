635f490d8b21034c916f7e11c028107a8a7841cb
Ankit Patel <ankitpatel1531@gmail.com>
2026-08-24 19:50:45 +0530

    ✅ QVQICP30 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

---
+++ codechef/medium/QVQICP30/solution.java
@@ -0,0 +1,7 @@
+class Codechef {
+    public static void main(String[] args) {
+        // write your code here4
+        System.out.println("My name is John.");
+        System.out.println("My favorite color is Blue.My favorite is Reading");
+    }
+}
