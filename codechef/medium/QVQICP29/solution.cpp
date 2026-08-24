b0007cdeed8d0aea1f20bc989a532997b318f37b
Ankit Patel <ankitpatel1531@gmail.com>
2026-08-24 19:42:45 +0530

    ✅ QVQICP29 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-24

---
+++ codechef/medium/QVQICP29/solution.cpp
@@ -0,0 +1,8 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        System.out.print("Hello, my name is Alex.");
+        System.out.print("Hello, my name is Bob.");
+    }
+}
+
