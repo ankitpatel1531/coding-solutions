81810ae5d90b40304040fbd56b106cbc890e6292
Ankit Patel <ankitpatel1531@gmail.com>
2026-08-24 18:40:49 +0530

    ✅ LJAAS20 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

---
+++ codechef/medium/LJAAS20/solution.java
@@ -0,0 +1,11 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	    System.out.println("1-1");
+	    System.out.println("2-4");
+	    System.out.println("3-9");
+	    System.out.println("4-16");
+	    System.out.println("5-25");
+	}
+}
