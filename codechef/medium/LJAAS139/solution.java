33bb5f25ac2d5fab88f4009813d5a7d5aa7cc282
Ankit Patel <ankitpatel1531@gmail.com>
2026-08-24 19:24:13 +0530

    ✅ LJAAS139 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

---
+++ codechef/medium/LJAAS139/solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int base = scanner.nextInt();
+        int exponent = scanner.nextInt();
+        
+        int result = calculatePower(base, exponent);
+        System.out.println(result);    
+    }
+    
+    public static int calculatePower(int base, int exponent) {
+        // Complete the method 
+        int result = 1;
+        for (int i =1;i<= exponent ;i++ ){
+            result = result*base;
+        } 
+        return result;
+        
+    }
+}
