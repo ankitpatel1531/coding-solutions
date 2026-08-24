6bb45a045f695226bf66783daa37f1e8ffc2fd2d
Ankit Patel <ankitpatel1531@gmail.com>
2026-08-24 19:14:33 +0530

    ✅ LJAAS105 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

---
+++ codechef/medium/LJAAS105/solution.java
@@ -0,0 +1,10 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		int[] array = {10, 12, 25, 9, 20};
+		System.out.println(array[0]*array[2]);
+        // Complete the code 
+        
+	}
+}
