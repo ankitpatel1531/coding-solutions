5c9c605bf2610ab746ccdcba899135b7fe956a4d
Ankit Patel <ankitpatel1531@gmail.com>
2026-08-28 01:31:38 +0530

    ✅ NOGJPU03 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-27

---
+++ codechef/medium/NOGJPU03/solution.java
@@ -0,0 +1,28 @@
+
+class ReadingTracker {
+    public static void main(String[] args) {
+        // Step 1: Declare and initialize totalPages and pagesRead
+        int totalPages = 350;
+        int pagesRead = 0;
+
+
+        // Step 2: Print initial status
+        System.out.println("Initial Reading Status:");
+        System.out.println("Total Pages: "+ totalPages+ ", Pages Read: "+pagesRead);
+
+        // Step 3: Update pagesRead after the first session (user read 50 pages)
+        pagesRead = pagesRead+50;
+
+        // Step 4: Print status after the first session
+        System.out.println("After first session: ");
+        System.out.println("Total Pages: "+ totalPages+ ", Pages Read: "+pagesRead);
+
+        // Step 5: Update pagesRead after the second session (user has read 75 more)
+        pagesRead = pagesRead+75;
+
+        // Step 7: Print final status
+        System.out.println("After second session:");
+        System.out.println("Total Pages: "+ totalPages+", Pages Read: "+pagesRead);
+        
+    }
+}
