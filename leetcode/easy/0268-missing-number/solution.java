97b54d395e1cf9ebe1cce6d42f44ba0be0580d5a
Ankit Patel <ankitpatel1531@gmail.com>
2026-08-27 13:53:35 +0530

    ✅ Missing Number | Easy | Java | Runtime: 135 ms (5.02%) | Memory: 47.1 MB | 2026-08-27

---
+++ leetcode/easy/0268-missing-number/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        for(int i =1;i<=n;i++){
+            int flag=0;
+            for(int j =0;j<n;j++){
+                if(nums[j] ==i){
+                    flag =1;
+                    break;
+                }
+            }if(flag ==0)
+                return i;
+            
+        }
+        return 0;
+        
+        
+    }
+}
