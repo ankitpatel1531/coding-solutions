bbe16214d303500e35473bbb2ed0411203fc2d5b
Ankit Patel <ankitpatel1531@gmail.com>
2026-08-28 01:35:50 +0530

    ✅ NOGJPU07 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-27

---
+++ codechef/medium/NOGJPU07/solution.java
@@ -0,0 +1,23 @@
+class PopulationTracker {
+    public static void main(String[] args) {
+ 
+        // TODO: Your code for writing cityname here
+        String cityname = "Springfield";
+ 
+      
+        // TODO: Your code for springfieldPopulation here
+        int springfieldPopulation=75320;
+        
+        // TODO: Your code for globalPopulation here
+        long globalPopulation=8123456789L;
+        
+        // TODO: Your print statement for cityname here 
+        System.out.println("City name : "+ cityname);
+       
+        // TODO: Your print statement for Springfield's population here
+        System.out.println("City Population: "+ springfieldPopulation);
+
+        // TODO: Your print statement for global population here
+        System.out.println("Global Population: "+globalPopulation);
+    }
+}
