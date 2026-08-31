d21552279ae7024fd9fcd21b390d648bfc2a7ca2
Ankit Patel <ankitpatel1531@gmail.com>
2026-08-31 22:39:21 +0530

    ✅ Largest Rectangle in Histogram | Hard | Java | Runtime: 68 ms (63.55%) | Memory: 78.2 MB | 2026-08-31

---
+++ leetcode/hard/0084-largest-rectangle-in-histogram/solution.java
@@ -0,0 +1,48 @@
+import java.util.Stack;
+
+class Solution {
+
+    public int largestRectangleArea(int[] heights) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        int maxArea = 0;
+
+        int n = heights.length;
+
+        for (int i = 0; i <= n; i++) {
+
+            int currentHeight;
+
+            if (i == n) {
+                currentHeight = 0;
+            } else {
+                currentHeight = heights[i];
+            }
+
+            while (!stack.isEmpty() &&
+                   currentHeight < heights[stack.peek()]) {
+
+                int height = heights[stack.pop()];
+
+                int width;
+
+                if (stack.isEmpty()) {
+                    width = i;
+                } else {
+                    width = i - stack.peek() - 1;
+                }
+
+                int area = height * width;
+
+                if (area > maxArea) {
+                    maxArea = area;
+                }
+            }
+
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+}
