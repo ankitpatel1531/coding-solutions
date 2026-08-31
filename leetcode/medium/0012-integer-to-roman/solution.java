2310d50dd00d365c7b50d1527e1b4060909fbf5d
Ankit Patel <ankitpatel1531@gmail.com>
2026-08-31 22:38:27 +0530

    ✅ Integer to Roman | Medium | Java | Runtime: 3 ms (99.81%) | Memory: 46.3 MB | 2026-08-31

---
+++ leetcode/medium/0012-integer-to-roman/solution.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public String intToRoman(int num) {
+        int[] val = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] s = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        StringBuilder result=new StringBuilder();
+
+        for(int i =0;i<val.length;i++){
+            while(num>=val[i]){
+                num = num-val[i];
+                result.append(s[i]);
+            }
+        }
+        return result.toString();
+
+        
+    }
+}
