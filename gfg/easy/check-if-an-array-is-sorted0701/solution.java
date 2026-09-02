ffe183046d73cc1d042e443e586a42f23ba3b9fa
Ankit Patel <ankitpatel1531@gmail.com>
2026-09-03 01:33:10 +0530

    ✅ Check Sorted Array | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ gfg/easy/check-if-an-array-is-sorted0701/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isSorted(int[] arr) {
+        for(int i =1;i<arr.length;i++){
+            if(arr[i]>= arr[i-1]){
+                continue;
+                
+            }
+            else{
+                return false;
+            }
+        }return true;
+        
+        
+/*        for(int i =0;i<arr.length-1;i++){
+            if(arr[i] > arr[i+1]) return false;
+        }
+        return true;
+        */
+        
+    }
+}
