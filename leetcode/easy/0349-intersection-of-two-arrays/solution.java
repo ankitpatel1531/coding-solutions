911da4308382833c60fb77d3bc68efb182c4e76a
Ankit Patel <ankitpatel1531@gmail.com>
2026-09-04 04:02:57 +0530

    ✅ Intersection of Two Arrays | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-09-03

--- leetcode/easy/0349-intersection-of-two-arrays/solution.java
+++ leetcode/easy/0349-intersection-of-two-arrays/solution.java
@@ -5,9 +5,9 @@ class Solution {
         ArrayList<Integer> list = new ArrayList<>();
 
         for(int i=0;i<nums1.length;i++){
-            // if(list.contains(nums1[i])) {
-            //     continue;
-            // }    
+            if(list.contains(nums1[i])) {
+                continue;
+            }    
             for(int j=0;j<nums2.length;j++){
                 if(nums1[i]==nums2[j] && vis[j]==0 ){
                     list.add(nums1[i]);
