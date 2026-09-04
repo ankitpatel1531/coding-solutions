077468c0ff90ee9f16be2a4afe3b91c6a029380f
Ankit Patel <ankitpatel1531@gmail.com>
2026-09-04 05:51:17 +0530

    ✅ Single Number | Easy | Java | Runtime: 13 ms (19.82%) | Memory: 46.9 MB | 2026-09-04

--- leetcode/easy/0136-single-number/solution.java
+++ leetcode/easy/0136-single-number/solution.java
@@ -1,11 +1,47 @@
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         int xor=0;
+//         for(int i =0;i<nums.length;i++){
+//             xor = xor^nums[i];
+//         }
+//         return xor;
+
+//     }
+// }
 class Solution {
     public int singleNumber(int[] nums) {
-        int xor=0;
 
-        for(int i =0;i<nums.length;i++){
-            xor = xor^nums[i];
+        HashMap<Long, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            long x = nums[i];
+
+            // map.put(x, map.getOrDefault(x, 0) + 1);
+
+            //or
+
+            // if (map.containsKey(x)) {
+            //     map.put(x, map.get(x) + 1);
+            // } else {
+            //     map.put(x, 1);
+            // }
+
+            // or
+            Integer count = map.get(x);
+
+            if (count == null) {
+                map.put(x, 1);
+            } else {
+                map.put(x, count + 1);
+            }
         }
-        return xor;
-        
+
+        for (long x : map.keySet()) {
+            if (map.get(x) == 1) {
+                return (int) x;
+            }
+        }
+
+        return -1;
     }
 }
