7f128194b402ce76658ee708694e7c925fc08559
Ankit Patel <ankitpatel1531@gmail.com>
2026-09-05 15:49:58 +0530

    ✅ Two Sum - Pair with Given Sum | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-05

---
+++ gfg/easy/key-pair5616/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i =0;i<arr.length;i++){
+            int x = arr[i];
+            
+            int required = target-x;
+            
+            if(map.containsKey(required)){
+                return true;
+            }
+            map.put(x,i);
+        }
+        return false;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    /*    int sum = 0;
+        for(int i =0;i<arr.length;i++){
+            for(int j = i+1;j<arr.length;j++){
+                if(arr[i] + arr[j] == target)
+                    return true;
+            }
+        }
+        return false;   */
+    }
+}
