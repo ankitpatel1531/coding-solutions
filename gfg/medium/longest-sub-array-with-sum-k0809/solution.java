113795e91f9f95013d76763a81bc27cf368574d4
Ankit Patel <ankitpatel1531@gmail.com>
2026-09-05 05:51:44 +0530

    ✅ Longest Subarray with Sum K | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-05

--- gfg/medium/longest-sub-array-with-sum-k0809/solution.java
+++ gfg/medium/longest-sub-array-with-sum-k0809/solution.java
@@ -1,6 +1,54 @@
 class Solution {
     public int longestSubarray(int[] arr, int k) {
         // code here
+       
+       
+    //   use only for non negative number
+    /*
+        int left= 0;
+        int sum =0;
+        int max=0;
+        
+        for(int right = 0;right< arr.length;right++){
+            sum = sum + arr[right];
+            
+            while(sum >k && left <= right){
+                sum = sum - arr[left];
+                left++;
+            }
+            if(sum ==k){
+                max = Math.max(max, right-left +1);
+            }
+        }
+        return max;
+*/        
+        
+
+        
+        
+        // use only non negative number
+        /*
+        int max = 0;
+        for(int i =0;i<arr.length;i++){
+            int sum =0;
+            
+            for(int j =i;j<arr.length;j++){
+                sum = sum+ arr[j];
+                
+                if(sum ==k){
+                    max= Math.max(max, j-i+1);
+                }
+            }
+        }
+        return max;  */
+        
+        
+        
+        
+        
+        
+        
+        
         HashMap<Long, Integer> map = new HashMap<>();
 
         long sum = 0;
@@ -30,6 +78,6 @@ class Solution {
             }
         }
 
-        return maxLen;
+        return maxLen;   
     }
 }
