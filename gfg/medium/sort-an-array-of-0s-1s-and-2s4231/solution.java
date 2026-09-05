1fb0a87ebc3858bdffe1e491bfc831b389c0bd12
Ankit Patel <ankitpatel1531@gmail.com>
2026-09-05 16:26:13 +0530

    ✅ Sort 0s, 1s and 2s | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-05

--- gfg/medium/sort-an-array-of-0s-1s-and-2s4231/solution.java
+++ gfg/medium/sort-an-array-of-0s-1s-and-2s4231/solution.java
@@ -1,7 +1,38 @@
 class Solution {
-    public void sort012(int[] arr) {
-        // code here
-        Arrays.sort(arr);
-        
-    }
+	public void sort012(int[] arr) {
+		
+		// Use dutch National flag Algorithm Use three pointers:
+		int low = 0;
+		int mid = 0;
+		int high = arr.length - 1;
+		
+		while (mid <= high) {
+			if (arr[mid] == 0) {
+				
+				int temp = arr[low];
+				arr[low] = arr[mid];
+				arr[mid] = temp;
+				
+				low++;
+				mid++;
+			}
+			else if(arr[mid] == 1){
+			    mid++;
+			}
+			else{
+			    // arr[mid]==2
+			    
+			    int temp = arr[mid];
+			    arr[mid] = arr[high];
+			    arr[high] = temp;
+			    high--;
+			    
+			}
+		}
+		
+		/*
+		Arrays.sort(arr);
+		*/
+		
+	}
 }
