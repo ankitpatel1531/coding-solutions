bac9fc637112c2289a4b20ea34e4ab99cb1e59d4
Ankit Patel <ankitpatel1531@gmail.com>
2026-09-12 16:09:49 +0530

    ✅ Count Rotations With Exactly K Equal Adjacent Pairs | Easy | Java | Runtime: 13 ms (27.64%) | Memory: 46.8 MB | 2026-09-12

---
+++ leetcode/easy/4043-count-rotations-with-exactly-k-equal-adjacent-pairs/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countRotations(String s, int k) {
+        int n = s.length();
+        int sol = 0;
+
+        for(int i =0;i<n;i++){
+            String rotated = s.substring(i)+ s.substring(0,i);
+
+            int score = 0;
+            for(int j =0;j<n-1;j++){
+                if(rotated.charAt(j) == rotated.charAt(j+1)){
+                    score++;
+                }
+            }if(score ==k){
+                sol++;
+            }
+        }
+        return sol;
+    }
+}
