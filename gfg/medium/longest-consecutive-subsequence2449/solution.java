3a606354a080ae259ffe588bce691fcf8fb65e43
Ankit Patel <ankitpatel1531@gmail.com>
2026-09-25 09:26:26 +0530

    ✅ Longest Consecutive Subsequence | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-25

--- gfg/medium/longest-consecutive-subsequence2449/solution.java
+++ gfg/medium/longest-consecutive-subsequence2449/solution.java
@@ -1,29 +1,65 @@
 class Solution {
 	public int longestConsecutive(int[] arr) {
-	    
-	    Arrays.sort(arr);
-	    int count =1;
-	    int longest = 1;
-	    
-	    for(int i =1;i<arr.length;i++){
-	        
-	        if(arr[i] == arr[i-1]){
-	            continue;
-	        }
-	        if(arr[i] == arr[i-1] +1){
-	            count ++;
-	        }
-	        else{
-	            count =1;
-	        }
-	        longest = Math.max(longest, count);
-	    }
-	    return longest;
-	    
-	    
-	    
-	   // Time Limit Exceeded 
-	/*	int longest = 0;
+		
+		HashSet<Integer> set = new HashSet<>();
+		
+		if (arr.length == 0) {
+			return 0;
+		}
+		
+		for (int num : arr) {
+			set.add(num);
+		}
+		
+		int longest = 1;
+		
+		for (int num : set) {
+			
+			if (!set.contains(num - 1)) {
+				
+				int current = num;
+				int count = 1;
+				
+				while (set.contains(current + 1)) {
+					current++;
+					count++;
+				}
+				
+				longest = Math.max(longest, count);
+			}
+		}
+		
+		return longest;
+		
+		
+		
+		// Better Approach
+		/*
+		Arrays.sort(arr);
+		int count = 1;
+		int longest = 1;
+		
+		for (int i = 1; i<arr.length; i++) {
+			
+			if (arr[i] == arr[i - 1]) {
+				continue;
+			}
+			if (arr[i] == arr[i - 1] +1) {
+				count ++;
+			}
+			else {
+				count = 1;
+			}
+			longest = Math.max(longest, count);
+		}
+		return longest;
+		*/
+		
+		
+		
+		
+		// Time Limit Exceeded
+		/*	int longest = 0;
 		for (int i = 0; i<arr.length; i++) {
 			int current = arr[i];
 			int count = 1;
